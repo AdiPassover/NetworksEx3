89d931039937728c840737faedacaf378b0bf720
Imri Shai <imrish149@gmail.com>
2024-02-25 15:37:49 +0200

    implemented sender and receiver

--- TCP_Receiver.c
+++ TCP_Receiver.c
@@ -1,9 +1,148 @@
+
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/time.h>
 
-int main(int argc, char *argv[]) {
-    printf("TCP_Receiver");
 
-    for (int i = 1; i < argc; i++) {
-        printf("%s\n",argv[i]);
+#define FILE_SIZE 2000000
+#define MAX_RUNS 100
+
+
+void printStats(double times[100], double speeds[100], int run);
+
+int main(int argc, char* argv[]) {
+    printf("Starting Reciver...\n");
+    if (argc != 5) {
+        printf("invalid command");
+        return 1;
     }
+    int port;
+    char algo[10] = {0};
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0) {
+            printf("%d\n",atoi(argv[i + 1]));
+            port = atoi(argv[i + 1]);
+        }
+        else if (strcmp(argv[i], "-algo") == 0) {
+            printf("%s\n",argv[i + 1]);
+           strcpy(algo, argv[i + 1]);
+        }
+    }
+    return 0;
+
+    struct sockaddr_in receiverAddress, senderAddress;
+    struct timeval start, end;
+
+    memset(&receiverAddress, 0, sizeof(receiverAddress));
+    memset(&senderAddress, 0, sizeof(senderAddress));
+    receiverAddress.sin_family = AF_INET;
+    receiverAddress.sin_addr.s_addr = INADDR_ANY;
+    receiverAddress.sin_port = htons(port);
+
+    int socketfd = -1;
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        perror("socket(2)");
+        //free(algo);
+        exit(1);
+    }
+    if (setsockopt(socketfd, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) != 0) {
+        perror("setsockopt(2)");
+        //free(algo);
+        exit(1);
+    }
+    //free(algo);
+
+    if (bind(socketfd, (struct sockaddr *) &receiverAddress, sizeof(receiverAddress)) == -1) {
+        perror("bind(2)");
+        exit(1);
+    }
+
+    if (listen(socketfd, 1) < 0) {
+        perror("listen(2)");
+        close(socketfd);
+        return 1;
+    }
+    socklen_t clientAddressLen = sizeof(senderAddress);
+
+    double times[MAX_RUNS];
+    double speeds[MAX_RUNS];
+    int currentRun = 1;
+
+    printf("Waiting for TCP connection...");
+
+    while (1) {
+
+        int client_sock = accept(socketfd, (struct sockaddr *) &senderAddress, &clientAddressLen);
+        if (client_sock < 0) {
+            perror("accept(2)");
+            close(socketfd);
+            return 1;
+        }
+        char *buffer[FILE_SIZE] = {0};
+        while (currentRun < MAX_RUNS) {
+            printf("Sender connected, beginning to receive file...\n");
+            gettimeofday(&start, NULL);
+            int bytes_received = recv(client_sock, buffer, FILE_SIZE, 0);
+            gettimeofday(&end, NULL);
+
+            if (strcmp((const char *) buffer, "exit") == 0) {
+                printf("Sender sent exit message.\n");
+                printStats(times, speeds, currentRun);
+                close(client_sock);
+                close(socketfd);
+                exit(1);
+            }
+            printf("File transfer completed.\n");
+
+            if (bytes_received < 0) {
+                perror("recv(2)");
+                close(client_sock);
+                close(socketfd);
+                return 1;
+            }
+
+            times[currentRun] = (end.tv_sec - start.tv_sec) * 1000.0;
+            times[currentRun] += (end.tv_usec - start.tv_usec) / 1000.0;
+            speeds[currentRun] = (bytes_received / times[currentRun]) / 1000;
+            currentRun++;
+            printf("Waiting for Sender response...");
+        }
+    }
+    return 0;
 }
+
+void printStats(double times[100], double speeds[100], int run) {
+    double timeSum = 0;
+    double sum1 = 0;
+    double avgTime = 0;
+    double avgSpeed = 0;
+    printf("--------------------------------------------\n");
+    printf("(*) Times summary:\n\n");
+    for (int i = 1; i < run; i++) {
+        timeSum += times[i];
+        printf("(*) Run %d, Time: %0.3lf ms\n", i, times[i]);
+    }
+    printf("\n(*) Speeds summary:\n\n");
+    for (int i = 1; i < run; i++) {
+        sum1 += speeds[i];
+        printf("(*) Run %d, Speed: %0.3lf KiB/s\n", i, speeds[i]);
+    }
+    if (run > 0) {
+        avgTime = (timeSum / (double) run);
+        avgSpeed = (sum1 / (double) run);
+    }
+    printf("\n(*) Time avarages:\n");
+    printf("(*) Avarage transfer time for whole file: %0.3lf ms\n", avgTime);
+    printf("(*) Avarage bandwidth: %0.3lf MB/s\n", avgSpeed);
+    printf("--------------------------------------------\n");
+
+}
+
+
--- TCP_Sender.c
+++ TCP_Sender.c
@@ -1,9 +1,122 @@
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include "stdlib.h"
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define FILE_SIZE 2000000
+
+/*
+* @brief A random data generator function based on srand() and rand().
+* @param size The size of the data to generate (up to 2^32 bytes).
+* @return A pointer to the buffer.
+*/
+char *util_generate_random_data(unsigned int size) {
+    char *buffer = NULL;
+    // Argument check.
+    if (size == 0)
+        return NULL;
+    buffer = (char *) calloc(size, sizeof(char));
+    // Error checking.
+    if (buffer == NULL)
+        return NULL;
+    // Randomize the seed of the random number generator.
+    srand(time(NULL));
+    for (unsigned int i = 0; i < size; i++)
+        *(buffer + i) = ((unsigned int) rand() % 256);
+    return buffer;
+}
 
 int main(int argc, char *argv[]) {
-    printf("TCP_Sender");
+    char *file = util_generate_random_data(FILE_SIZE);
 
+    char *SERVER_IP = (char *) malloc(sizeof(char) * (16));
+    int port;
+    char *algo = (char *) malloc(sizeof(char) * 10);
     for (int i = 1; i < argc; i++) {
-        printf("%s\n",argv[i]);
+        if (strcmp(argv[i], "-p") == 0) { port = atoi(argv[i + 1]); }
+        else if (strcmp(argv[i], "-ip") == 0) { strcpy(SERVER_IP, argv[i + 1]); }
+        else if (strcmp(argv[i], "-algo") == 0) { strcpy(algo, argv[i + 1]); }
     }
+
+    struct sockaddr_in senderAdress;
+    memset(&senderAdress, 0, sizeof(senderAdress));
+    senderAdress.sin_family = AF_INET;
+    senderAdress.sin_addr.s_addr = INADDR_ANY;
+    senderAdress.sin_port = htons(port);
+
+    int socketfd = -1;
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        perror("socket(2)");
+        free(SERVER_IP);
+        free(algo);
+        exit(1);
+    }
+    if (setsockopt(socketfd, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) != 0) {
+        perror("setsockopt(2)");
+        free(SERVER_IP);
+        free(algo);
+        exit(1);
+    }
+    free(algo);
+
+    if (inet_pton(AF_INET, SERVER_IP, &senderAdress.sin_addr) <= 0) {
+        perror("inet_pton(3)");
+        close(socketfd);
+        free(SERVER_IP);
+        return 1;
+    }
+
+    if (connect(socketfd, (struct sockaddr *) &senderAdress, sizeof(senderAdress)) < 0) {
+        perror("connect(2)");
+        close(socketfd);
+        free(SERVER_IP);
+        return 1;
+    }
+
+    long bytes_sent = send(socketfd, file, FILE_SIZE, 0);
+    if (bytes_sent <= 0) {
+        perror("send(2)");
+        close(socketfd);
+        free(SERVER_IP);
+        return 1;
+    }
+    int input = -1;
+
+    while (input) {
+
+        printf("Press 1 to resend the file, press 0 to exit.");
+        scanf("%d", &input);
+        if (input == 1) {
+            bytes_sent = send(socketfd, file, FILE_SIZE, 0);
+            if (bytes_sent <= 0) {
+                perror("send(2)");
+                close(socketfd);
+                free(SERVER_IP);
+                return 1;
+            }
+        }
+        else{
+            bytes_sent = send(socketfd, "exit", 4, 0);
+            if (bytes_sent <= 0)
+            {
+                perror("send(2)");
+                close(socketfd);
+                free(SERVER_IP);
+                return 1;
+            }
+            close(socketfd);
+            free(SERVER_IP);
+            return 0;
+        }
+    }
+
+    free(SERVER_IP);
+    return 0;
 }
+
